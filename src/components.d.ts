e14162a1ebb6c077832eb28ca51dad6af508cad4
Tommaso De Marco <tommaso.de-marco@odt.net>
2021-03-23 18:08:44 +0100

    Add changeCubePosition function

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,56 +6,32 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface ThreeD {
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLThreeDElement extends Components.ThreeD, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLThreeDElement: {
+        prototype: HTMLThreeDElement;
+        new (): HTMLThreeDElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "three-d": HTMLThreeDElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface ThreeD {
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "three-d": ThreeD;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "three-d": LocalJSX.ThreeD & JSXBase.HTMLAttributes<HTMLThreeDElement>;
         }
     }
 }
--- src/components/three-d/three-d.tsx
+++ src/components/three-d/three-d.tsx
@@ -1,15 +1,56 @@
-import { Component, Host, h } from "@stencil/core"
-
+import { Component, Host, State, h } from "@stencil/core"
+import * as THREE from "three"
 @Component({
     tag: "three-d",
     styleUrl: "three-d.scss",
     shadow: false
 })
 export class ThreeD {
+
+    @State() scene: any;
+    @State() mesh: any;
+    @State() renderer: any;
+    @State() camera: any;
+
+    private sizes = {
+        width: 800,
+        height: 500
+    }
+    axesHelper: any;
+
+    componentWillLoad() {
+        this.axesHelper = new THREE.AxisHelper(4)
+        this.scene = new THREE.Scene()
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({ color: "#fff" })
+        this.mesh = new THREE.Mesh(geometry, material)
+        this.scene.add(this.mesh)
+        this.camera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height)
+        this.camera.position.z = 6
+        this.scene.add(this.camera)
+        this.scene.add(this.axesHelper)
+    }
+
+    componentDidLoad() {
+        const canvas = document.querySelector("canvas")
+        this.renderer = new THREE.WebGLRenderer({ canvas: canvas })
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+        this.renderer.render(this.scene, this.camera)
+    }
+
+    private changeCubePosition = () => {
+        this.mesh.position.set(1, 0.7, -2)
+        this.mesh.scale.set(2, 0.3, 2)
+        this.mesh.rotation.y = 1.4
+        this.camera.position.set(1, 2, 1)
+        this.renderer.render(this.scene, this.camera)
+    }
+
     render() {
         return (
             <Host>
-                <h1>Hello from the component</h1>
+                <h1 onClick={this.changeCubePosition}>Hello from the component</h1>
+                <canvas class="canvas"></canvas>
             </Host>
         )
     }
